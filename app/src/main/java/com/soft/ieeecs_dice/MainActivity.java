16ac2a6575793ea84a6d3c7187756fa6a7f86914
Gokul-Coding <gokulpro1210@gmail.com>
2025-03-09 16:44:23 +0530

    Working Version

--- app/src/main/java/com/soft/ieeecs_dice/MainActivity.java
+++ app/src/main/java/com/soft/ieeecs_dice/MainActivity.java
@@ -16,11 +16,40 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int random_int;
+    private Button roll_button;
+    private ImageView image;
+    private ArrayList<Integer> imagearray;
+
+    Random obj = new Random();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        roll_button = findViewById(R.id.rollbutton);
+        image = findViewById(R.id.imageView);
+
+        imagearray = new ArrayList<>();
+        imagearray.add(R.drawable.dice1);
+        imagearray.add(R.drawable.dice2);
+        imagearray.add(R.drawable.dice3);
+        imagearray.add(R.drawable.dice4);
+        imagearray.add(R.drawable.dice5);
+        imagearray.add(R.drawable.dice6);
+
+        image.setImageResource(imagearray.get(0));
+
+        roll_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                random_int = obj.nextInt(6);
+                image.setImageResource(imagearray.get(random_int));
+            }
+        });
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -28,5 +57,4 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
-
 }
